9dc513ed4e2ea700df20ef77f938c364aa534f4e
Manish Kumar <55046229+krmanu07@users.noreply.github.com>
2024-10-21 11:37:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
 
+# Set to store unique visitor IPs
+visitor_ips = set()
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    visitor_ip = request.remote_addr  # Get the visitor's IP address
+    visitor_ips.add(visitor_ip)  # Add the IP to the set of visitors
+    return render_template('index.html')  # Render your HTML template
 
+@app.route('/visitor_count')
+def visitor_count():
+    # Return the number of unique visitors as JSON
+    return jsonify({'current_visitors': len(visitor_ips)})
 
 if __name__ == '__main__':
     app.run(debug=True)
